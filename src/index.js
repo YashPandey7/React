5f82c9c293821a65fdf1c3f70d435afb418c93b6
Yash Pandey <yashdevesh10@gmail.com>
2022-07-18 20:50:55 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World!!</h1> , document.getElementById("root"));
+ReactDOM.render(
+    <>
+        <h1>Hello World!!</h1> 
+        <p>It's Yash Pandey</p>
+        <a href="#">click me</a>
+    </>
+, document.getElementById("root"));
