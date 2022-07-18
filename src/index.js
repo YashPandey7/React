0ddac02368426863ded30a7e0127a97e3073d34e
Yash Pandey <yashdevesh10@gmail.com>
2022-07-18 21:14:37 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
     <>
-        <h1>Top 5 Netflix Series (personal choice)</h1> 
-        <p>- yash pandey </p>
-        <ol>
-            <li>Breaking Bad</li>
-            <li>Stranger Things</li>
-            <li>Money Heist</li>
-            <li>squid game</li>
-            <li>Lucifer</li>
-        </ol>
+        <h1>Hello World!!</h1> 
+        <p>It's Yash Pandey</p>
+        <a href="#">click me</a>
     </>
 , document.getElementById("root"));
