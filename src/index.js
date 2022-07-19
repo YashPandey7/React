036fa941c1e7fdedab01de30322d8865482919a9
Yash Pandey <yashdevesh10@gmail.com>
2022-07-20 01:02:26 +0530

    Adding 4th branch

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const fname = "Dark" , lname = "Batman" ;
+const d = new Date();
 
 ReactDOM.render(
     <>
         <h1>{`My name is ${fname} ${lname}`}</h1>
         <p>{`But you can call me just "${lname}" 😂`}</p>
+        <p>{`Todays Date is ${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`}</p>
+        <p>{`Todays Time is ${d.toLocaleTimeString()}`}</p>
     </>
 , document.getElementById("root"));
