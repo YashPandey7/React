bb7fdff12e069697e358354ba31373ac691361e0
Yash Pandey <yashdevesh10@gmail.com>
2022-07-22 13:35:58 +0530

    Netflix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App, {name ,nm} from './App';
+import App from './App';
 
 ReactDOM.render(
-    <>
-        <App/>
-        <ol>
-            <li>{name}</li>
-            <li>{nm}</li>
-        </ol>
-    </>
+    <App/>
     , document.getElementById('root')
 );
