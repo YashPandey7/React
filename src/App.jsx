1ded597ed770d1717d002698484c2b37d657ecb5
Yash Pandey <yashdevesh10@gmail.com>
2022-07-29 01:14:10 +0530

    input

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
 import React, { useState } from 'react';
 
-function App (){
-    const time = new Date().toLocaleTimeString();
-    const [exacttime ,updatetime] = useState(time);
+const App = () => 
+{
+    const [name , setname]= useState('');
+    const [clkname , setclk ] =useState('');
 
-    const onclick = () =>{
-        const time = new Date().toLocaleTimeString();
-        updatetime(time);
+    const inputevent1 = (event) => {
+        setname(event.target.value);
     }
-    
-    setInterval(onclick , 1000);
+
+    const [email ,setemail] = useState('');
+    const [clkemail , setclkemail] = useState('');
+    const inputevent2 = (event) => {
+        setemail(event.target.value);
+    }
+
+    const onclk = (a) =>{
+        a.preventDefault();
+        setclk(name);
+        setclkemail(email);
+    }
 
     return(
         <>
-            <div>
-                <h1>Time {exacttime}</h1>
-                {/* <button onClick={onclick}>Get Time</button> */}
-            </div>
+        <div style={{textAlign:'center'}}>
+        <form onSubmit={onclk}>
+            <h1>
+                {`Hi ${clkname}`} <br/> {`Your email Id is ${clkemail}`}
+            </h1>
+            <input type='text' placeholder='Enter your name...' onChange={inputevent1} value={name}/>
+            <br/><br/>
+            <input type='text' placeholder='Enter your email...' onChange={inputevent2} value={email}/>
+            <br/><br/>
+            <button type='submit'>Submit</button>
+        </form>
+        </div>                        
         </>
     );
 }
