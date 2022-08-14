f8c1d22f603ba83bada6583dfcb30e386efcb4c8
Yash Pandey <yashdevesh10@gmail.com>
2022-08-15 01:14:36 +0530

    as

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,22 @@
 import React, { useState } from 'react';
+import './index.css';
 
-const App = () => 
-{
-    const [fullname , setfullname]= useState({
-        fname : '',
-        lname : '',
-        email : '',
-        phone : ''
-    } );
+const App = () => {
+    const a = new Date().toLocaleTimeString();
+    const [Time, setTime] = useState(a);
 
-    const inputevent = (event) => {
-        // console.log(event.target.value);
-        // console.log(event.target.name);
+    const onclk = () => {
+        const a = new Date().toLocaleTimeString();
+        setTime(a);
+    }
 
-        const value = event.target.value;
-        const name = event.target.name;
-        
-        setfullname((b) => {
-            if(name === 'n_fname'){
-               return{
-                fname : value,
-                lname : b.lname,
-                email : b.email,
-                phone : b.phone
-               };
-            }
-            else if(name === 'n_lname'){
-                return{
-                    fname : b.fname,
-                    lname : value,
-                    email : b.email,
-                    phone : b.phone 
-                };
-            }
-            else if(name === 'n_email'){
-                return{
-                    fname : b.fname,
-                    lname : b.lname ,
-                    email : value,
-                    phone : b.phone
-                };
-            }
-            else if(name === 'n_number'){
-                return{
-                    fname : b.fname,
-                    lname : b.lname,
-                    email : b.email,
-                    phone : value 
-                };
-            }
-        })
-    };
+    setInterval(onclk,1000);
 
-    const onclk = (a) =>{
-        a.preventDefault();
-        alert('Submitted successfully');
-    };
-
-    return(
+    return (
         <>
-        <div style={{textAlign:'center'}}>
-        <form onSubmit={onclk}>
-            <h1>
-                {`Hi ${fullname.fname} ${fullname.lname}`}
-            </h1>
-            <p>Your Email is {fullname.email}</p>
-            <p>Your Phone Number is {fullname.phone}</p>
-            <input type='text' placeholder='Enter your first name...' onChange={inputevent} name='n_fname'/>
-            <br/><br/>
-            <input type='text' placeholder='Enter your Last name...' onChange={inputevent} name='n_lname'/>
-            <br/><br/>
-            <input type='email' placeholder='Enter your Email...' onChange={inputevent} name='n_email'/>
-            <br/><br/>
-            <input type='number' placeholder='Enter your Phone number...' onChange={inputevent} name='n_number'/>
-            <br/><br/>
-            <button type='submit'>Submit</button>
-        </form>
-        </div>                        
+            <div style={{fontSize:'28px'}}>{Time}</div>
+            <br/>
+            {/* <button onClick = {onclk}>Update</button> */}
         </>
     );
 }
